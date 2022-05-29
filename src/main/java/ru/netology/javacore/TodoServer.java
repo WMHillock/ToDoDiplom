06ec0c2e598c5b6cdc18b3ccac9bf3b8d6500066
WMHillock <bugorskiyvv@gmail.com>
2022-05-29 14:56:57 +0300

    Added dependencies Writed TODOs

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -8,14 +8,17 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    private int port;
+    private Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
+        ServerSocket server = new ServerSocket(port);
+        // TODO описываем работу сервера
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -4,18 +4,18 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+    //TODO как храним данные
 
     public void addTask(String task) {
-        //...
+        //TODO что проверяем и как добавляем таски
     }
 
     public void removeTask(String task) {
-        //...
+        //TODO что проверяем и как удаляем таски
     }
 
     public String getAllTasks() {
-        //...
+        //TODO выводим список всех тасков по алфавиту (Stream naturalOrder)
     }
 
 }
